e278473cba3d1d52565a6b43147ef6d428875edb
Maxwell <maxwstull@gmail.com>
2023-08-24 10:17:31 -0500

    Removed useless stuff, integrated new class for reading

--- comms.py
+++ comms.py
@@ -20,44 +20,17 @@
 #    print(value) # printing the value
 openhardwaremonitor_hwtypes = ['Mainboard','SuperIO','CPU','RAM','GpuNvidia','GpuAti','TBalancer','Heatmaster','HDD']
 openhardwaremonitor_sensortypes = ['Voltage','Clock','Temperature','Load','Fan','Flow','Control','Level','Factor','Power','Data','SmallData','Throughput']
-import os 
-import clr
 import time
-clr.AddReference(r'OpenHardwareMonitorLib')
+from hardwareLogger import hardwareLogger
 
-from OpenHardwareMonitor import Hardware
-handle = Hardware.Computer()
-handle.MainboardEnabled = True
-handle.CPUEnabled = True
-handle.RAMEnabled = True
-handle.GPUEnabled = True
-handle.HDDEnabled = True
-handle.Open()
-
-
-dicty = {}
-from classes import OHardware, OSensor, CQueue
-## once
-for i in handle.Hardware: # Iterate over each hardware component (CPU, GPU, RAM)
-    i.Update() # Gets updated values
-    new_hw = OHardware(i.Identifier, i.Name, i.HardwareType, i.Parent) # Create hardware object
-    print(new_hw)    
-    for sensor in i.Sensors: # Iterate over each hardware sensor (temperature, clock, load)
-        if sensor.Value is not None: # Duh
-            # Create the sensor object (something like CPU Core 1 Temperature)
-            new_sensor = OSensor(sensor.Name,sensor.SensorType,sensor.Value,sensor.Index, sensor.Identifier)
-            new_hw.add_sensor(str(sensor.SensorType), new_sensor)
-    dicty[i.Identifier] = new_hw
-print("Initialization Completed")
+hw = hardwareLogger()
 
 for _ in range(0,60): # Loop to read
-    time.sleep(0.1) # Leave a gap
-    for i in handle.Hardware:
-        i.Update()
-        recalled = dicty[i.Identifier]
-        for sensor in i.Sensors:
-            if sensor.Value is not None:
-                recalled.sens_ids[sensor.Identifier].log(sensor.Value)
+    time.sleep(0.5)
+    start = time.time()
+    hw.read()
+    end = time.time()
+    print("Read duration: {t:4.4}s".format(t=end-start))
 
-for key, value in dicty.items():
+for key, value in hw.hardware_obj_dict.items():
     print(value.short())

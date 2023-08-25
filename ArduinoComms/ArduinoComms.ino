8ad3ef421e80fb7932b58cd9f06ae249175d4f0f
Maxwell <maxwstull@gmail.com>
2023-08-25 09:35:38 -0500

    Made servos work (yipee!)

--- ArduinoComms/ArduinoComms.ino
+++ ArduinoComms/ArduinoComms.ino
@@ -24,6 +24,7 @@ void serialEvent()
     Serial.write(ch);
   }
 }*/
+/*
 int x;
 void setup() {
  Serial.begin(115200);
@@ -33,4 +34,54 @@ void loop() {
  while (!Serial.available());
  x = Serial.readString().toInt();
  Serial.print(x + 1);
+}
+*/
+#include <Servo.h>
+Servo CT;
+Servo GT;
+Servo RL;
+Servo CL;
+Servo GL;
+int angle = 0;
+void setup() {
+  CT.attach(6);
+  GT.attach(9);
+  RL.attach(10);
+  CL.attach(11);
+  GL.attach(13);
+/*
+  CT.write(90);
+  GT.write(90);
+  RL.write(90);
+  CL.write(90);
+  GL.write(90);
+*/
+  CT.write(135);
+  GT.write(170);
+  RL.write(120);
+  CL.write(120);
+  GL.write(120);
+  delay(2000);
+
+  CT.write(45);
+  GT.write(10);
+  RL.write(60);
+  CL.write(60);
+  GL.write(60);
+  delay(2000);
+
+  CT.write(135);
+  GT.write(170);
+  RL.write(120);
+  CL.write(120);
+  GL.write(120);
+  delay(2000);
+
+  GT.detach();
+  
+}
+
+void loop()
+{
+
 }

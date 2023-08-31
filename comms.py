800992bc5e26e3ceabbaa600d8387d9b1ac99be2
Maxwell <maxwstull@gmail.com>
2023-08-31 15:40:25 -0500

    Made reading temps and sending values run in psuedo-parallel

--- comms.py
+++ comms.py
@@ -21,7 +21,8 @@
 openhardwaremonitor_hwtypes = ['Mainboard','SuperIO','CPU','RAM','GpuNvidia','GpuAti','TBalancer','Heatmaster','HDD']
 openhardwaremonitor_sensortypes = ['Voltage','Clock','Temperature','Load','Fan','Flow','Control','Level','Factor','Power','Data','SmallData','Throughput']
 
-REFRESH_RATE_HZ = 1
+READ_REFRESH_RATE_HZ = 4
+SEND_REFRESH_RATE_HZ = 0.5
 
 
 import time
@@ -31,9 +32,18 @@ from hardwareLogger import hardwareLogger
 from serialComm import serialComm
 import serial.tools.list_ports
 
-async def main():
-    refresh_period_sec = 1/REFRESH_RATE_HZ
-    hw = hardwareLogger()
+
+
+async def read_coroutine(hw):
+    period = 1/READ_REFRESH_RATE_HZ
+    while(True):
+        start = time.time()
+        hw.read()
+        print("[RC] Values updated", time.time())
+        await asyncio.sleep(period - (time.time() - start))
+
+async def send_coroutine(hw):
+    period = 1/SEND_REFRESH_RATE_HZ
     found_port = False
     portname = ""
     ctr = 0
@@ -41,30 +51,30 @@ async def main():
         ports = serial.tools.list_ports.comports()
         for port in ports:
             if 'Arduino' in port[1]:
-                print("Arduino port identified: ", port[0])
+                print("[SC] Arduino port identified: ", port[0])
                 portname = port[0]
                 found_port = True
-        asyncio.sleep(1)
+        await asyncio.sleep(1)
         if ctr % 5 == 0:
-            print("Attempting to identify to Arduino port")
-
+            print("[SC] Attempting to identify to Arduino port", time.time())
+        ctr += 1
     arduino = serialComm(port=portname, baud=9600, timeout=1)
-
-    for _ in range(0,100): # Loop to read
+    while(True):
         start = time.time()
-        hw.read()
-
         to_send = hw.get_values_to_send()
         to_send_str = "{:3}{:3}{:3}{:3}{:3}".format(to_send[0], to_send[1], to_send[2], to_send[3], to_send[4])
-        print("Sending", to_send)
+        print("[SC] Sending", to_send, " ", time.time())
         result = arduino.write_read(to_send_str)
         if(result == b'1'):
             pass 
         else:
-            print("Ak not received")
+            print("[SC] Ak not received", time.time())
+        await asyncio.sleep(period - (time.time() - start))
 
-
-        await asyncio.sleep(refresh_period_sec - (time.time() - start))
+async def main():
+    hw = hardwareLogger()
+    await asyncio.gather(read_coroutine(hw), send_coroutine(hw))
 
 if __name__ == "__main__":
     asyncio.run(main())
+    
